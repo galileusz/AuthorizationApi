2b6cbd49a18bc89fee30dae6421cc03cbb2eadcf
galileusz <mgalas91@gmail.com>
2024-07-05 20:57:20 +0200

    Repair solution warnings (ammend)

--- AuthManager.API/DAL/Entities/RefreshToken.cs
+++ AuthManager.API/DAL/Entities/RefreshToken.cs
@@ -6,7 +6,7 @@ public class RefreshToken
 {
     [Key]
     public int Id { get; set; }
-    public string Token { get; set; }
-    public string UserId { get; set; }
+    public string Token { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
     public DateTime ExpiryDate { get; set; }
 }
